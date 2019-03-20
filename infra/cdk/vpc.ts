2b4af2fc373bb0c739ac54e68c08f3e4b6a9812c
gongxiaoliang <gongxiaoliang@mobifun365.net>
2019-03-20 18:09:19 +0800

    new RDS

--- infra/cdk/vpc.ts
+++ infra/cdk/vpc.ts
@@ -1,20 +1,20 @@
 import ec2 = require('@aws-cdk/aws-ec2');
-// import rds = require('@aws-cdk/aws-rds')
+import rds = require('@aws-cdk/aws-rds')
 // import { InstanceType } from '@aws-cdk/aws-ec2';
 import cdk = require('@aws-cdk/cdk');
 
-class RDS extends cdk.Stack {
+export class RDS extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // const vpc = new ec2.VpcNetwork(this, 'GpayVpc', { 
 
-    new ec2.VpcNetwork(this, 'GpayVpc', { 
+      const vpc = new ec2.VpcNetwork(this, 'GpayVpc', { 
         cidr: "10.0.0.0/16", 
         maxAZs: 2 ,
         subnetConfiguration: [
             {
-              cidrMask: 26,
+              cidrMask: 20,
               name: 'Public',
               subnetType: ec2.SubnetType.Public,
             },
@@ -23,29 +23,27 @@ class RDS extends cdk.Stack {
               name: 'Private',
               subnetType: ec2.SubnetType.Private,
             }
-            // {
-            //   cidrMask: 27,
-            //   name: 'Database',
-            //   subnetType: ec2.SubnetType.Isolated,
-            // }
         ],
         natGateways: 1,
     });
 
-    // new rds.DatabaseCluster(this, 'Database', {
-    //     engine: rds.DatabaseClusterEngine.Aurora,
-    //     masterUser: {
-    //         username: 'root',
-    //         password: 'Mobifun365',
-    //     },
-    //     instanceProps: {
-    //         instanceType: new ec2.InstanceTypePair(ec2.InstanceClass.Burstable2, ec2.InstanceSize.Small),
-    //         vpcPlacement: {
-    //             subnetsToUse: ec2.SubnetType.Public,
-    //         },
-    //         vpc
-    //     }
-    // });
+    new rds.DatabaseCluster(this, 'Database', {
+        engine: rds.DatabaseClusterEngine.Aurora,
+        masterUser: {
+            username: 'root',
+            password: 'Mobifun365',
+        },
+        instanceProps: {
+            instanceType: new ec2.InstanceTypePair(ec2.InstanceClass.Burstable2, ec2.InstanceSize.Small),
+            vpcPlacement: {
+                subnetsToUse: ec2.SubnetType.Private,
+            },
+            vpc
+        },
+        port: 3306,
+        defaultDatabaseName: 'gpay',
+        instances: 1
+    });
 
   }
 }
